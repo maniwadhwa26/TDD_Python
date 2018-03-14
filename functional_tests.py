8567221a8d4a821e160af9a560fb6df91ed8fe9b
Mani Wadhwa <maniwadhwa26@gmail.com>
2018-03-15 00:24:42 +0530

    Functional test change. Created separate function to check for multiple text

--- functional_tests.py
+++ functional_tests.py
@@ -8,7 +8,12 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
-    
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -25,11 +30,14 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
+        #table = self.browser.find_element_by_id('id_list_table')
         
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), f"New to-do item did not appear in table. Contents were:\n{table.text}")
-        self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+        #rows = table.find_elements_by_tag_name('tr')
+        #self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), f"New to-do item did not appear in table. Contents were:\n{table.text}")
+        #self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
         self.fail('Finish the test!')
 
 if __name__ == "__main__":
