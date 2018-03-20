958f72d9b3e8cfb474c83c3ced9f2bbdc16e1706
Mani Wadhwa <maniwadhwa26@gmail.com>
2018-03-21 00:18:06 +0530

    set STATIC_ROOT in settings and disable admin

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
+import os
  
 MAX_WAIT = 10
  
@@ -14,6 +15,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
     live_server_url = 'http://localhost:8000'
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://'+staging_server
 
     def tearDown(self):
         self.browser.quit()
