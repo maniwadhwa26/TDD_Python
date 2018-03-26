d176a599dd0de09d4af370cca2b3da3f2fc94155
Mani Wadhwa <maniwadhwa26@gmail.com>
2018-03-26 23:57:24 +0530

    Adjust new list view to do model validation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,7 +7,7 @@ from unittest import skip
 import time
 import os
  
-MAX_WAIT = 10
+MAX_WAIT = 20
  
  
 #class NewVisitorTest(LiveServerTestCase):
@@ -29,6 +29,19 @@ class FunctionalTest(StaticLiveServerTestCase):
         while True:
             try:
                 table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')  
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
