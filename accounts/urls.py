b73862431bc6326e8e739741662f7efde151ef2d
Mani Wadhwa <maniwadhwa26@gmail.com>
2018-04-01 19:34:44 +0530

    logout url add

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import logout
 from django.conf.urls import include, url
-from accounts import views
+from accounts import views  
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     url(r'^login$', views.login, name='login'),
